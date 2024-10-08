702352afe9f4713a6b5df1bac509df49c0abd732
AlexisSanchez-24 <alexis.sanchez@genstudents.org>
2024-10-08 15:26:35 -0300

    Commit 08.10.2024v10

---
+++ Red-Neuronal-Basica/celsius-a-farenheitv2.py
@@ -0,0 +1,38 @@
+import tensorflow as tf;
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+
+
+celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float);
+farenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float);
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1]);
+oculta2 = tf.keras.layers.Dense(units=3);
+salida = tf.keras.layers.Dense(units=1);
+modelo = tf.keras.Sequential([oculta1, oculta2, salida]);
+
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+    """optimizador y perdida, permite saber a la red ajustar los pesos y los sesgos, de manera eficiente para que aprenda y no desaprenda,valor numerico que corresponde a la taza de aprendizaje si es un numero muy pequeño aprendera muy lento si el numero es muy grande se pasara de lo esperado
+    mean_squared_error: es la funcion que revisa errores que indica que una poco cantidad de errores gandres es peor que muchos errores pequeños
+    """
+);
+
+print("Comenzando entrenamiento...");
+historial = modelo.fit(celsius, farenheit, epochs=1000, verbose=False); #entrenamiento de la red 1000: corresponde a las veces que repetira el entrenamiento
+print("Modelo entrenado!");
+ 
+plt.xlabel("# Epoca");
+plt.ylabel("Magnitud de perdida");
+plt.plot(historial.history["loss"]); #grafica de la perdida
+
+print("Hagamos una prediccion!");
+resultado = modelo.predict([100.0]); #prediccion
+print("El resultado es " + str(resultado) + " farenheit!");
+
+print("Variables internas del modelo")
+print(oculta1.get_weights());
+print(oculta2.get_weights());
+print(salida.get_weights());
